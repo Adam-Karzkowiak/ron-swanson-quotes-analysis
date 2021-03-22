1fee997b1c8799c94d4746674f6942c967f82b00
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-03-22 23:15:37 +0100

    method to connect, get and edit Ron Swanson quotes (take it easy - not their content, but just removing the square brackets and quotation marks)

--- src/main/java/service/QuotesProvider.java
+++ src/main/java/service/QuotesProvider.java
@@ -1,4 +1,28 @@
 package service;
 
-class QuotesProvider {
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class QuotesProvider {
+
+    public String connectAndGetAmazingQuote() throws Exception {
+        URL url = new URL(Constants.RON_SWANSON_QUOTES_URL);
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer content = new StringBuffer();
+        while ((inputLine = in.readLine()) != null) {
+            content.append(inputLine);
+        }
+        in.close();
+        con.disconnect();
+        String solution = content.toString();
+        solution = solution.replaceAll("[\\[\\]\"]", "");
+        return solution;
+    }
 }
