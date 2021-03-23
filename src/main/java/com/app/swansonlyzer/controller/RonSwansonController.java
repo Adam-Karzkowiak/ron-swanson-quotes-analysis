d9eddce9ae01d838da1b9e6a0079c9ebf907d004
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-03-23 23:15:09 +0100

    Simple controller created. Its workin!

---
+++ src/main/java/com/app/swansonlyzer/controller/RonSwansonController.java
@@ -0,0 +1,29 @@
+package com.app.swansonlyzer.controller;
+
+import com.app.swansonlyzer.service.Analyzer;
+import com.app.swansonlyzer.service.QuotesProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+@RestController
+class RonSwansonController {
+
+    private final Analyzer analyzer;
+    private final QuotesProvider quotesProvider;
+
+    @Autowired
+    RonSwansonController(final Analyzer analyzer, final QuotesProvider quotesProvider) {
+        this.analyzer = analyzer;
+        this.quotesProvider = quotesProvider;
+    }
+
+
+    @GetMapping("/ron")
+    public ArrayList<String> analyzeRon() throws Exception {
+        String solution = quotesProvider.connectAndGetAmazingQuote();
+        return analyzer.analyse(solution);
+    }
+}
