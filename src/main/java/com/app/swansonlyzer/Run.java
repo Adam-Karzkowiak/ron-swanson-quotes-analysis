505d89e3416ab0eef8f52607d3507f7644b17ebb
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-03-23 19:24:08 +0100

    Run with Spring

---
+++ src/main/java/com/app/swansonlyzer/Run.java
@@ -0,0 +1,13 @@
+package com.app.swansonlyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Run {
+    public static void main(String[] args) {
+        SpringApplication.run(Run.class, args);
+
+    }
+}
